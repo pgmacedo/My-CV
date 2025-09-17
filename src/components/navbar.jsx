62fbf662af436f8b1a33a52dacdf78888b0bd1ba
Paulo Macedo <paulogranjamacedo@gmail.com>
2025-09-18 00:34:35 +0100

    nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -109,7 +109,7 @@ function ResponsiveAppBar() {
             variant="h5"
             noWrap
             component="a"
-            href="xxxx"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
