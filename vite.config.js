7be4b50d76f06ad9d9fd2bf5a75a3fb6a43ea2fc
Paulo Macedo <paulogranjamacedo@gmail.com>
2025-09-17 21:20:38 +0100

    change base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 replace with your repo name
+  base: '/My-CV/', // 👈 replace with your repo name
 })
