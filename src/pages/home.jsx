4f5c167d26d44f56793bad12c9eb0bb399d79432
Paulo Macedo <paulogranjamacedo@gmail.com>
2025-09-21 16:34:42 +0100

    xx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,27 +2,19 @@ import { supabase } from '../lib/supabase'
 import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const [users, setUsers] = useState([])
+  const { data } = supabase
+    .storage
+    .from('CV')
+    .getPublicUrl('CV.pdf')
 
-  useEffect(() => {
-    async function loadUsers() {
-      const { data, error } = await supabase.from('notes').select('*')
-      if (error) console.error(error)
-      else setUsers(data)
-    }
-    loadUsers()
-  }, []);
   return (
     <>
       <h1>My CV</h1>
       <iframe
-        src="/CV.pdf"
+        src={data.publicUrl}
         title="CV PDF - Paulo Macedo"
         style={{ width: '90vw', height: '90vh', border: '1px solid #ccc' }}
       />
-      <ul>
-        {users.map(u => <li key={u.id}>{u.title}</li>)}
-      </ul>
     </>
   );
 }
