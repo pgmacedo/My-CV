ffb7ecc99022f6b3d2e2c76055d6108e5786c687
Paulo Macedo <paulogranjamacedo@gmail.com>
2025-09-21 15:40:18 +0100

    l

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <h1>My CV</h1>
       <iframe
         src="/CV.pdf"
-        title="CV PDF"
+        title="CV PDF - Paulo Macedo"
         style={{ width: '90vw', height: '90vh', border: '1px solid #ccc' }}
       />
     </>
